a04739b4d2e17679781beef71a26a244264d4539
drmystery1 <166465142+drmystery1@users.noreply.github.com>
2026-08-22 23:42:32 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tele-tuty-v70';
+const CACHE='tele-tuty-V72';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method==='GET'){e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))}});
